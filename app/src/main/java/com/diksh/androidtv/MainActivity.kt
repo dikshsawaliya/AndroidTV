550fa91261a51f25a77b0109d34d3d17b1cc9807
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2023-07-18 16:15:01 +0530

    added the leanback library in the manifest, disabled the touch controls

--- app/src/main/java/com/diksh/androidtv/MainActivity.kt
+++ app/src/main/java/com/diksh/androidtv/MainActivity.kt
@@ -3,7 +3,7 @@ package com.diksh.androidtv
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity() {   //replaced AppcompatActivity with the FragmentActivity because action bar is not used
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
